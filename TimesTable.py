c3e9ff35cfefa22f7ecf034e9b24b76e85d756a0
Jusstiinn <S10273684@connect.np.edu.sg>
2025-07-28 15:40:32 +0800

    This is my first commit

---
+++ TimesTable.py
@@ -0,0 +1,9 @@
+#Justin Chua, IM02, S10273684F
+
+number = int(input("Please enter a number: "))
+
+for i in range(1,11):
+    answer = number * i
+    print(f"{number} x {i} = {answer}")
+
+print("The End")
